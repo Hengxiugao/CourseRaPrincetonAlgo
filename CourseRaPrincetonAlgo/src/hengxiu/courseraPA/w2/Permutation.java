ac618083d53db85acf7b6f95279cb7c2bb42b6b8
Hengxiugao <hengxiugao@gmail.com>
2017-02-24 01:37:54 -0600

    Bugs fixed

--- CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w2/Permutation.java
+++ CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w2/Permutation.java
@@ -1,11 +1,8 @@
 package hengxiu.courseraPA.w2;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Iterator;
 
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
 	
@@ -14,9 +11,7 @@ public class Permutation {
 	public static void main(String[] args) {
 		rQ = new RandomizedQueue<String>();
 		int k = Integer.parseInt(args[0]);
-		String fileName = args[2];
-		String content = readLineFromFile(fileName);
-		readDeque(content);
+		rQ.enqueue(StdIn.readString());
 		if (k > rQ.size()) {
 			k = rQ.size();
 		}
@@ -26,37 +21,4 @@ public class Permutation {
 		}
 		
 	}
-	
-	private static void readDeque(String content) {
-		StringBuffer sb = new StringBuffer(content);
-		while (sb.indexOf(" ") > 0) {
-			rQ.enqueue(sb.substring(0, sb.indexOf(" ")));
-			sb.delete(0, sb.indexOf(" ") + 1);
-		}
-		rQ.enqueue(sb.toString());
-	}
-	
-	private static String readLineFromFile(String fileName) {
-		BufferedReader br = null;
-		String line = "";
-		try {
-			br = new BufferedReader(new FileReader(fileName));
-		    
-		    line = br.readLine();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-		    try {
-		    	if (br != null) {
-		    		br.close();
-		    	}
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return line;
-	}
-
 }
