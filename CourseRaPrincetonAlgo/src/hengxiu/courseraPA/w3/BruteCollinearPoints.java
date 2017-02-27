5a6fc01a583e482c3fe67d8f46fbaf31da06ab0d
hengxiugao <hengxiugao@gmail.com>
2017-02-26 22:54:40 -0600

    BruteCollinearPoints

--- CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w3/BruteCollinearPoints.java
+++ CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w3/BruteCollinearPoints.java
@@ -1,18 +1,21 @@
 package hengxiu.courseraPA.w3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class BruteCollinearPoints {
 	
 	List<Point> pointList;
-	
+	List<LineSegment> segList;
 	// finds all line segments containing 4 points
 	public BruteCollinearPoints(Point[] points) {
 		if (points == null) {
 			throw new java.lang.NullPointerException();
 		}
 		pointList = new ArrayList<>();
+		segList = new ArrayList<>();
 		for (Point p : points) {
 			if (p == null) {
 				throw new java.lang.NullPointerException();
@@ -22,15 +25,34 @@ public class BruteCollinearPoints {
 			}
 			pointList.add(p);
 		}
+		
+		int n = pointList.size();
+		for (int i = 0; i < n - 3; i++) {
+			 for (int j = i + 1; j < n - 2; j++) {
+				 for (int k = j + 1; j < n - 1; k++) {
+					 for (int l = k + 1; l < n; l++) {
+						 Point p = pointList.get(i);
+						 Point q = pointList.get(j);
+						 Point s = pointList.get(k);
+						 Point r = pointList.get(l);
+						 if (p.slopeTo(q) == p.slopeTo(s) && p.slopeTo(q) == p.slopeTo(r)) {
+							 ArrayList<Point> tList = new ArrayList<>(Arrays.asList(p, q, s, r));
+							 Collections.sort(tList);
+							 segList.add(new LineSegment(tList.get(0), tList.get(tList.size() - 1)));
+						 }
+					 }
+				 }
+			 }
+		 } 
 	}
 	
 	// the number of line segments
 	public int numberOfSegments() {
-		
+		return segList.size();
 	}
 	
-	 // the line segments
+	 // the line segments;
 	 public LineSegment[] segments() {
-		 
+		 return segList.toArray(new LineSegment[segList.size()]);
 	 }
 }
