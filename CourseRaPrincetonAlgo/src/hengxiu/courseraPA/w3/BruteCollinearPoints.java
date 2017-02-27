b4807c649a18fb8aa7bd4fbc2e3a5b88d20322bf
hengxiugao <hengxiugao@gmail.com>
2017-02-26 23:36:34 -0600

    Bugs fixed

--- CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w3/BruteCollinearPoints.java
+++ CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w3/BruteCollinearPoints.java
@@ -29,7 +29,7 @@ public class BruteCollinearPoints {
 		int n = pointList.size();
 		for (int i = 0; i < n - 3; i++) {
 			 for (int j = i + 1; j < n - 2; j++) {
-				 for (int k = j + 1; j < n - 1; k++) {
+				 for (int k = j + 1; k < n - 1; k++) {
 					 for (int l = k + 1; l < n; l++) {
 						 Point p = pointList.get(i);
 						 Point q = pointList.get(j);
