771ff37512d9df86f11e44110870a6a401ee0cf0
hengxiugao <hengxiugao@gmail.com>
2017-03-04 01:45:37 -0600

    Correct version

--- CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w4/Board.java
+++ CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w4/Board.java
@@ -6,12 +6,16 @@ import java.util.Iterator;
 public class Board {
 	
 	private int[][] blocks;
-	private int neighborIndex = 0;
 	private int zeroCol = 0;
 	private int zeroRow = 0;
+	private int move;
 	// construct a board from an n-by-n array of blocks
 	// (where blocks[i][j] = block in row i, column j)
 	public Board(int[][] blocks) {
+		this(blocks, 0);
+	}
+	
+	private Board(int[][] blocks, int move) {
 		this.blocks = new int[blocks.length][blocks.length];
 		for (int i = 0; i < dimension(); i++) {
 			for (int j = 0 ; j < dimension(); j++) {
@@ -22,6 +26,7 @@ public class Board {
 				}
 			}
 		}
+		this.move = move;
 	}
 	
 	// board dimension n
@@ -59,7 +64,7 @@ public class Board {
 				result += Math.abs(targetRow - i) + Math.abs(targetCol - j);
 			}
 		}
-		return result;
+		return result + move;
 	}
 	
 	// is this board the goal board?
@@ -83,61 +88,20 @@ public class Board {
 	
 	// a board that is obtained by exchanging any pair of blocks
 	public Board twin() {
-		int[][] neighborBlocks = new int[blocks.length][blocks.length];
+		int[][] twinBlocks = new int[blocks.length][blocks.length];
 		
 		for (int i = 0; i < dimension(); i++) {
 			for (int j = 0 ; j < dimension(); j++) {
-				neighborBlocks[i][j] = blocks[i][j];
-				if (blocks[i][j] == 0) {
-					zeroRow = i;
-					zeroCol = j;
-				}
+				twinBlocks[i][j] = blocks[i][j];
 			}
 		}
 		
-		switch (neighborIndex) {
-			case 0 :
-				//Swap with up
-				neighborIndex++;
-				if (zeroRow >= 1) {
-					swap(neighborBlocks, zeroRow, zeroCol, zeroRow - 1, zeroCol);
-				} else {
-					return twin();
-				}
-				break;
-			
-			case 1 :
-				//Swap with down
-				neighborIndex++;
-				if (zeroRow < dimension() - 1) {
-					swap(neighborBlocks, zeroRow, zeroCol, zeroRow + 1, zeroCol);
-				} else {
-					return twin();
-				}
-				break;
-			
-			case 2 :
-				//Swap with left
-				neighborIndex++;
-				if (zeroCol >= 1) {
-					swap(neighborBlocks, zeroRow, zeroCol, zeroRow, zeroCol - 1);
-				} else {
-					return twin();
-				}
-				break;
-			case 3 :
-				//Swap with right
-				neighborIndex++;
-				if (zeroCol < dimension() - 1) {
-					swap(neighborBlocks, zeroRow, zeroCol, zeroRow, zeroCol + 1);
-				} else {
-					return twin();
-				}
-				break;
-			default:
-				return null;
+		if (zeroRow == 0) {
+			swap(twinBlocks, 1, 0, 1, 1);
+		} else {
+			swap(twinBlocks, 0, 0, 0, 1);
 		}
-		return new Board(neighborBlocks);
+		return new Board(twinBlocks);
 	}
 	
 	private void swap(int[][] arr, int i1, int j1, int i2, int j2) {
@@ -163,23 +127,45 @@ public class Board {
 	
 	// all neighboring boards
 	public Iterable<Board> neighbors() {
-		return new BoardNeighboring();
-	}
-	
-	private class BoardNeighboring implements Iterable<Board>{
-
-		@Override
-		public Iterator<Board> iterator() {
-			ArrayList<Board> boardList = new ArrayList<>();
-			Board nextBoard = twin();
-			while (nextBoard != null) {
-				boardList.add(nextBoard);
-				nextBoard = twin();
+		int[][] neighborBlocks = new int[blocks.length][blocks.length];
+		ArrayList<Board> boardList = new ArrayList<>();
+		
+		for (int i = 0; i < dimension(); i++) {
+			for (int j = 0 ; j < dimension(); j++) {
+				neighborBlocks[i][j] = blocks[i][j];
+				if (blocks[i][j] == 0) {
+					zeroRow = i;
+					zeroCol = j;
+				}
 			}
-			return boardList.iterator();
 		}
 		
+		if (zeroRow >= 1) {
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow - 1, zeroCol);
+			boardList.add(new Board(neighborBlocks, move + 1));
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow - 1, zeroCol);
+		}
+		if (zeroRow < dimension() - 1) {
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow + 1, zeroCol);
+			boardList.add(new Board(neighborBlocks, move + 1));
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow + 1, zeroCol);
+		}
+		if (zeroCol >= 1) {
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow, zeroCol - 1);
+			boardList.add(new Board(neighborBlocks, move + 1));
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow, zeroCol - 1);
+		}
+		if (zeroCol < dimension() - 1) {
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow, zeroCol + 1);
+			boardList.add(new Board(neighborBlocks, move + 1));
+			swap(neighborBlocks, zeroRow, zeroCol, zeroRow, zeroCol + 1);
+		}
+		
+		return boardList;
 	}
+	
+	
+
 	
 	// string representation of this board (in the output format specified below)
 	public String toString() {
@@ -197,7 +183,12 @@ public class Board {
 	// unit tests (not graded)
 	public static void main(String[] args) {
 		Board testBoard = new Board(new int[][]{{0, 1, 3}, {4, 2, 5}, {7, 8, 6}});
-		System.out.println(testBoard);
+		System.out.println("Board:\n"+testBoard);
+		System.out.println("Twin Board:\n"+testBoard.twin());
+		System.out.println("Neighbours:");
+		for (Board b : testBoard.neighbors()) {
+			System.out.println(b);
+		}
 		System.out.println("score: hamming = " + testBoard.hamming() + ", manhattan = " + testBoard.manhattan() + ", isGoal=" + testBoard.isGoal());
 		for (Board b : testBoard.neighbors()) {
 			System.out.println(b);
