9c280f4aee4360a749cfc034c014d51595806c30
hengxiugao <hengxiugao@gmail.com>
2017-03-12 13:59:58 -0500

    PointSet

--- CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w5/PointSET.java
+++ CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w5/PointSET.java
@@ -1,38 +1,44 @@
 package hengxiu.courseraPA.w5;
 
+import java.util.TreeSet;
+
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class PointSET {
+	TreeSet<Point2D> pointSet;
+	
 	// construct an empty set of points 
 	public PointSET() {
-		
+		pointSet = new TreeSet<>();
 	}
 	
 	// is the set empty? 
 	public boolean isEmpty() {
-		
+		return pointSet.isEmpty();
 	}
 	
 	// number of points in the set 
 	public int size() {
-		
+		return pointSet.size();
 	}
 	
 	// add the point to the set (if it is not already in the set)
 	public void insert(Point2D p) {
-		
+		pointSet.add(p);
 		
 	}   
 	
 	// does the set contain point p? 
 	public boolean contains(Point2D p) {
-		
+		return pointSet.contains(p);
 	}   
 	
 	// draw all points to standard draw
 	public void draw() {
-		
+		for (Point2D p : pointSet) {
+			p.draw();
+		}
 	}                 
 	
 	// all points that are inside the rectangle 
