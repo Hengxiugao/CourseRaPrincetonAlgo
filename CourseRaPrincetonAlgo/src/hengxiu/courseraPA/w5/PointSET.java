0ba37a42021bddd48534d9872fedd8378d474c0f
hengxiugao <hengxiugao@gmail.com>
2017-03-12 21:38:23 -0500

    PointSet brute force

--- CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w5/PointSET.java
+++ CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w5/PointSET.java
@@ -1,5 +1,7 @@
 package hengxiu.courseraPA.w5;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeSet;
 
 import edu.princeton.cs.algs4.Point2D;
@@ -25,12 +27,18 @@ public class PointSET {
 	
 	// add the point to the set (if it is not already in the set)
 	public void insert(Point2D p) {
+		if (p == null) {
+			throw new java.lang.NullPointerException();
+		}
 		pointSet.add(p);
 		
 	}
 	
 	// does the set contain point p? 
 	public boolean contains(Point2D p) {
+		if (p == null) {
+			throw new java.lang.NullPointerException();
+		}
 		return pointSet.contains(p);
 	}
 	
@@ -43,12 +51,33 @@ public class PointSET {
 	
 	// all points that are inside the rectangle 
 	public Iterable<Point2D> range(RectHV rect) {
-		
+		if (rect == null) {
+			throw new java.lang.NullPointerException();
+		}
+		List<Point2D> resultList = new ArrayList<>();
+		for (Point2D p : pointSet) {
+			if (rect.distanceSquaredTo(p) == 0) {
+				resultList.add(p);
+			}
+		}
+		return resultList;
 	}
 	
 	// a nearest neighbor in the set to point p; null if the set is empty 
 	public Point2D nearest(Point2D p) {
-		
+		if (p == null) {
+			throw new java.lang.NullPointerException();
+		}
+		double min = Double.MAX_VALUE;
+		Point2D minPoint = null;
+		for (Point2D point : pointSet) {
+			double curDis = point.distanceTo(p);
+			if (min > curDis) {
+				min = curDis;
+				minPoint = point;
+			}
+		}
+		return minPoint;
 	}
 	
 	// unit testing of the methods (optional) 
