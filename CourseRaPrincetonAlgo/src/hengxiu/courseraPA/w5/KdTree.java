4947307b3a3ce19f3c0908d14779818213fd7a7e
Hengxiugao <hengxiugao@gmail.com>
2017-03-13 18:48:24 -0500

    insertTo method updated

--- CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w5/KdTree.java
+++ CourseRaPrincetonAlgo/src/hengxiu/courseraPA/w5/KdTree.java
@@ -1,12 +1,16 @@
 package hengxiu.courseraPA.w5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
-	
+	KdNode root;
+	int size;
 	private class KdNode {
-		//Odd level : vertical, even level : horizontal
+		//Odd level : horizontal, even level : vertical
 		int level;
 		Point2D point;
 		KdNode left;
@@ -19,43 +23,107 @@ public class KdTree {
 	
 	// construct an empty set of points 
 	public KdTree() {
-		
+		root = null;
+		size = 0;
 	}
 	
 	// is the set empty? 
 	public boolean isEmpty() {
-		
+		return size() == 0;
 	}
 	
 	// number of points in the set 
 	public int size() {
-		
+		return size();
 	}
 	
 	// add the point to the set (if it is not already in the set)
 	public void insert(Point2D p) {
+		if (p == null) {
+			throw new java.lang.NullPointerException();
+		}
+		if (isEmpty()) {
+			root = new KdNode(0, p);
+		} else {
+			insertTo(root, p);
+		}
 		
+	}
+	
+	private void insertTo(KdNode root, Point2D p) {
+		if (root == null) {
+			return;
+		}
+		//Vertical level, compare x
+		if (root.level % 2 == 0) {
+			//target point is on the left of the root point
+			if (p.x() < root.point.x()) {
+				if (root.left == null) {
+					root.left = new KdNode(root.level + 1, p);
+					return;
+				}
+				insertTo(root.left, p);
+			} else {
+			//target point is on the right of the root point
+				if (root.right == null) {
+					root.right = new KdNode(root.level + 1, p);
+					return;
+				}
+				insertTo(root.right, p);
+			}
+		} else {
+		//Horizontal level, compare y
+
+			//target point is on the down of the root point
+			if (p.y() < root.point.y()) {
+				if (root.left == null) {
+					root.left = new KdNode(root.level + 1, p);
+					return;
+				}
+				insertTo(root.left, p);
+			} else {
+			//target point is on the up of the root point
+				if (root.right == null) {
+					root.right = new KdNode(root.level + 1, p);
+					return;
+				}
+				insertTo(root.right, p);
+			}
 		
-	}   
+		}
+	}
 	
 	// does the set contain point p? 
 	public boolean contains(Point2D p) {
-		
-	}   
+		if (p == null) {
+			throw new java.lang.NullPointerException();
+		}
+	}
+
 	
 	// draw all points to standard draw
 	public void draw() {
-		
-	}                 
+		traverseAndDraw(root);
+	}
+	
+	private void traverseAndDraw(KdNode root) {
+		traverseAndDraw(root.left);
+		root.point.draw();
+		traverseAndDraw(root.right);
+	}
 	
 	// all points that are inside the rectangle 
 	public Iterable<Point2D> range(RectHV rect) {
-		
-	}   
+		if (rect == null) {
+			throw new java.lang.NullPointerException();
+		}
+	}
 	
 	// a nearest neighbor in the set to point p; null if the set is empty 
 	public Point2D nearest(Point2D p) {
-		
+		if (p == null) {
+			throw new java.lang.NullPointerException();
+		}
 	}
 	
 	// unit testing of the methods (optional) 
